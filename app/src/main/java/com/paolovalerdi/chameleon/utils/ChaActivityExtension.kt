2ebe659f18959c605c82a99f8b02798badbdad47
Paolo Valerdi <paolovalerdi@gmail.com>
2020-04-11 15:54:15 -0500

    Edge to edge won't override previous set flags

--- app/src/main/java/com/paolovalerdi/chameleon/utils/ChaActivityExtension.kt
+++ app/src/main/java/com/paolovalerdi/chameleon/utils/ChaActivityExtension.kt
@@ -1,46 +1,55 @@
 package com.paolovalerdi.chameleon.utils
 
 import android.app.Activity
-import android.graphics.Color
 import android.os.Build
 import android.view.View
 import androidx.annotation.ColorInt
+import dev.jorgecastillo.androidcolorx.library.HSLAColor
+import dev.jorgecastillo.androidcolorx.library.asColorInt
+import dev.jorgecastillo.androidcolorx.library.asHsla
+
+
+internal inline fun Activity.updateSystemUiVisibility(block: (flags: Int) -> Int) {
+    window.decorView.systemUiVisibility = block(window.decorView.systemUiVisibility)
+}
 
 fun Activity.enableEdgeToEdge() {
-    window.decorView.systemUiVisibility =
-        View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
-                View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or
+    updateSystemUiVisibility { flags ->
+        flags or View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
+                //View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or
                 View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+    }
 }
 
 fun Activity.enableLightStatusBar(enabled: Boolean) {
-    var flags = window.decorView.systemUiVisibility
     if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-        flags = if (enabled) {
-            flags or View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
-        } else {
-            flags and View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR.inv()
+        updateSystemUiVisibility { flags ->
+            if (enabled) {
+                flags or View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
+            } else {
+                flags and View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR.inv()
+            }
         }
-        window.decorView.systemUiVisibility = flags
     }
 }
 
 fun Activity.setNavigationBarColor(@ColorInt color: Int) {
-    val fixedColor = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-        color
-    } else Color.BLACK
+    val fixedColor = if (Build.VERSION.SDK_INT < Build.VERSION_CODES.O) {
+        val hsla = color.asHsla()
+        HSLAColor(hsla.hue, hsla.saturation, 0.4f, hsla.alpha).asColorInt()
+    } else color
     window.navigationBarColor = fixedColor
     enableLightNavigationBar(fixedColor.isLight())
 }
 
 fun Activity.enableLightNavigationBar(enabled: Boolean) {
-    var flags = window.decorView.systemUiVisibility
     if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-        flags = if (enabled) {
-            flags or View.SYSTEM_UI_FLAG_LIGHT_NAVIGATION_BAR
-        } else {
-            flags and View.SYSTEM_UI_FLAG_LIGHT_NAVIGATION_BAR.inv()
+        updateSystemUiVisibility { flags ->
+            if (enabled) {
+                flags or View.SYSTEM_UI_FLAG_LIGHT_NAVIGATION_BAR
+            } else {
+                flags and View.SYSTEM_UI_FLAG_LIGHT_NAVIGATION_BAR.inv()
+            }
         }
-        window.decorView.systemUiVisibility = flags
     }
 }
